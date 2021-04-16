3e01b75bb913409f19a232492f5c74a35b711702
LukeWalter <lwalter5667@gmail.com>
2021-04-16 19:28:00 -0400

    Added documentation for TestDriver.java

--- src/test/TestDriver.java
+++ src/test/TestDriver.java
@@ -7,49 +7,128 @@ public final class TestDriver {
 
     public static void main(String[] args) {
 
-        // Tape t = new Tape("Tape 1");
-        // System.out.println(t);
-        // t.move('_', 'a', 'R');
-        // System.out.println(t);
-        // t.move('_', 'a', 'R');
-        // System.out.println(t);
-        // t.move('_', 'a', 'R');
-        // System.out.println(t);
+        /*  Title of the TM  */
+        String title = "{a^nb^n | n >= 0}";
+
+        /** 
+         *  Non-null set of states for the TM.
+         *  Each state must have a unique name,
+         *  and the set must contain a state
+         *  named "qa", which is used as the
+         *  accepting state for the TM. The
+         *  "qr" state, while not required, is
+         *  another special state that is used
+         *  as the rejecting state when included.
+         */
+        String[] Q = {"q0", "q1", "q2", "q3", "q4", "qa", "qr"};
+        
+
+        /**
+         *  Non-null input alphabet for the TM.
+         *  This variable determines which 
+         *  characters are allowed to be
+         *  used in an input string. Each 
+         *  character must be unique and 
+         *  be a letter, number, or special
+         *  character. '_' cannot be used
+         *  here as it is reserved for the
+         *  stack alphabet.
+         */
+        Character[] Σ = {'a', 'b'};
+        
+
+        /**
+         *  Non-null stack alphabet for the TM.
+         *  This variable determines which
+         *  characters are allowed to be
+         *  written to this TM's tapes. This
+         *  set must contain every character
+         *  from the input alphabet as well as
+         *  the '_' symbol, which is used to
+         *  denote an empty slot on the tape.
+         *  Other characters may be added here
+         *  as needed.
+         */
+        Character[] T = {'_', 'a', 'b', 'x'};
+
+
+        /* Number of tapes for the TM */
+        int tapes = 1;
+        
+
+        /**
+         *  Non-null set of moves for the TM.
+         *  This variable can store three
+         *  different characters ('L', 'R',
+         *  and 'S'), which represent the 
+         *  actions of moving left on a tape,
+         *  moving right on a tape, and 
+         *  staying in place, respectively.
+         */
+        Character[] moves = {'L', 'R'};
+        
+
+        /**
+         *  Initial state for the TM.
+         *  This state must be included
+         *  in Q.
+         */
+        String initialState = "q0";
+
+
+        /**
+         *  Set of all transition functions used
+         *  by the TM. 
+         * 
+         *  General form for a transition function:
+         * 
+         *      initialState->targetState: readChar->writeChar, move;
+         * 
+         *  The initial and target states for the
+         *  transition function must exist in Q,
+         *  the characters that are read and
+         *  written must exist in T, and the move must
+         *  exist in moves.
+         * 
+         *  When using multiple tapes, the form will follow as such:
+         * 
+         *      2-tape | initialState->targetState: readChar->writeChar, move // readChar->writeChar, move;
+         *      3-tape | initialState->targetState: readChar->writeChar, move // readChar->writeChar, move // readChar->writeChar, move;
+         *      ...                                 (tape 1)                     (tape 2)                     (tape 3)
+         * 
+         */
+        Transition[] d = {
+            new Transition("q0->q1: a->_,R;", tapes),
+            new Transition("q1->q1: a->a,R;", tapes),
+            new Transition("q1->q1: x->x,R;", tapes),
+            new Transition("q1->qr: _->_,R;", tapes),
+            new Transition("q1->q2: b->x,R;", tapes),
+            new Transition("q2->q2: b->b,R;", tapes),
+            new Transition("q2->q3: _->_,L;", tapes),
+            new Transition("q3->q3: a->a,L;", tapes),
+            new Transition("q3->q3: b->b,L;", tapes),
+            new Transition("q3->q3: x->x,L;", tapes),
+            new Transition("q3->q4: _->_,R;", tapes),
+            new Transition("q4->q4: x->x,R;", tapes),
+            new Transition("q4->q1: a->x,R;", tapes),
+            new Transition("q0->qr: b->_,R;", tapes),
+            new Transition("q4->qr: b->_,R;", tapes),
+            new Transition("q2->qr: a->a,R;", tapes),
+            new Transition("q0->qa: _->_,R;", tapes),
+            new Transition("q4->qa: _->_,R;", tapes)
+        };
+
 
         TuringMachine tm = new TuringMachine(
-            "{a^nb^n | n >= 0}", 
-            new String[]{"q0", "q1", "q2", "q3", "q4", "qa", "qr"}, 
-            new Character[]{'a', 'b'}, 
-            new Character[]{'_', 'a', 'b', 'x'}, 
-            1, 
-            new Character[]{'L', 'R'}, 
-            "q0", 
-            new Transition[] {
-                new Transition("q0->q1: a->_,R;", 1),
-                new Transition("q1->q1: a->a,R;", 1),
-                new Transition("q1->q1: x->x,R;", 1),
-                new Transition("q1->qr: _->_,R;", 1),
-                new Transition("q1->q2: b->x,R;", 1),
-                new Transition("q2->q2: b->b,R;", 1),
-                new Transition("q2->q3: _->_,L;", 1),
-                new Transition("q3->q3: a->a,L;", 1),
-                new Transition("q3->q3: b->b,L;", 1),
-                new Transition("q3->q3: x->x,L;", 1),
-                new Transition("q3->q4: _->_,R;", 1),
-                new Transition("q4->q4: x->x,R;", 1),
-                new Transition("q4->q1: a->x,R;", 1),
-                new Transition("q0->qr: b->_,R;", 1),
-                new Transition("q4->qr: b->_,R;", 1),
-                new Transition("q2->qr: a->a,R;", 1),
-                new Transition("q0->qa: _->_,R;", 1),
-                new Transition("q4->qa: _->_,R;", 1)
-
-            }
+            title, Q, Σ, T, tapes, 
+            moves, initialState, d
         );
 
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Choose an input string to simulate: ");
         tm.simulate(sc.nextLine());
+
         sc.close();
 
     } // main
